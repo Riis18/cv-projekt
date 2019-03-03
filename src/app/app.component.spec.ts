b3be053da354f657670723eb3d6366305c9227c3
Riis18 <Riisjesper@hotmail.com>
2019-03-04 00:10:35 +0100

    fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Tina-CV'`, () => {
+  it(`should have as title 'Jesper-CV'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Tina-CV');
+    expect(app.title).toEqual('Jesper-CV');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Tina-CV!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Jesper-CV!');
   });
 });
